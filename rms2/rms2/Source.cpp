a9e08b6756596e8191c740c1cf557215bbaf12a9
NISHI3 <nishi3@NISHI3-Air.local>
2017-05-17 18:28:25 +0900

    fix rms2

--- rms2/rms2/Source.cpp
+++ rms2/rms2/Source.cpp
@@ -3,13 +3,13 @@ int main(void) {
 	int a, b, c, d;
 	for (a = 0; a < 100; ++a) {
 		b = a % 3, c = a % 5, d = a % 15;
-		if (d == 0) {
+		if (b == 0) {
 			printf("fizz\n");
 		}
 		else if (c == 0) {
 			printf("buzz\n");
 		}
-		else if (b == 0) {
+		else if (d == 0) {
 			printf("fizzbuzz\n");
 		}
 		else {
