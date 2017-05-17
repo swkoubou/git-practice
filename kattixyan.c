a79efd94d8d1c0d89f5cb9b1e413f8f6a008eb91
NISHI3 <nishi3@NISHI3-Air.local>
2017-05-17 18:03:51 +0900

    fix kattixyan.c

--- kattixyan.c
+++ kattixyan.c
@@ -5,14 +5,14 @@ int main(void) {
 	do {
 
 		--a;
-		printf("%d", a);
+		printf("%d:   ", a);
 		if (a % 15==0)
 			printf("fizzbuzz");
 		
 		else if (a % 3 == 0)
-			printf("fizz", a);
+			printf("fizz");
 		else if (a % 5 == 0)
-			printf("buzz", a);
+			printf("buzz");
 		printf("\n");
 	
 
